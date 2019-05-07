9a73b497d219b5b43edcd803ffe4b34eea7d1b7b
Adam McMaster <adam@zooniverse.org>
2019-05-07 13:23:50 +0100

    Turn off debugging and use gunicorn in staging

--- start_server.sh
+++ start_server.sh
@@ -8,7 +8,7 @@ done
 echo Applying migrations
 python manage.py migrate --noinput
 
-if [ "$DJANGO_ENV" == "production" ]; then
+if [ "$DJANGO_ENV" == "production" ] || [ "$DJANGO_ENV" == "staging" ]; then
   if [ ! -d "hamlet/static" ]; then
     echo Generating static files
     python manage.py collectstatic
