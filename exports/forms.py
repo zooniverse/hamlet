723e973a7eb53238c6bd945a236ecd2e712102c1
Adam McMaster <adam@zooniverse.org>
2019-06-24 14:28:56 +0100

    UI work
    - Refactor templates
    - Add instructions to project page
    - Require POST to generate exports
    
    Closes #49

--- exports/forms.py
+++ exports/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
+
 class WorkflowExportForm(forms.Form):
     storage_prefix = forms.CharField(
         label='Google Cloud Storage prefix',
         max_length=256,
+        widget=forms.TextInput(attrs={'placeholder': 'gs://my-storage-bucket'}),
     )
